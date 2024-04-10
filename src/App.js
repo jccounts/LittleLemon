ea64c23d083275f84f2066beb4db770387823a12
jccounts <github@bytecounts.com>
2024-04-10 14:46:37 -0700

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
     return (
         <React.Fragment>
             <Navbar/>
+
+            <Footer/>
         </React.Fragment>
     );
 }
